76801b865e9b73e581e8e24c2b6ff5506a800166
teanyun1864 <teanyun1864@gmail.com>
2024-07-16 22:20:28 +0900

    검색창 구현

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // 프로젝트 데이터 저장용 배열
+    var projects = [];
+
     // Quill editor 설정
     var quill = new Quill('#editor-container', {
-        theme: 'snow'
+        theme: 'snow',
+        modules: {
+            toolbar: [
+                [{ 'font': [] }],
+                [{ 'header': [1, 2, 3, false] }],
+                ['bold', 'italic', 'underline'],
+                [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                [{ 'color': [] }, { 'background': [] }],
+                [{ 'align': [] }],
+                ['link', 'image', 'video'],
+                ['clean'] 
+            ]
+        }
     });
 
     // 폼 제출 이벤트 핸들러
@@ -19,14 +34,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // 프로젝트 리스트에 프로젝트 추가 함수
     function addProjectToList(name, description) {
+        // 프로젝트 데이터 배열에 추가
+        projects.push({ name: name, description: description });
+        updateProjectList();
+    }
+
+    // 프로젝트 리스트 업데이트 함수
+    function updateProjectList() {
         var projectContainer = document.getElementById('project-container');
-        var projectItem = document.createElement('li');
-        projectItem.classList.add('project-item');
-        projectItem.innerText = name;
-        projectItem.addEventListener('click', function () {
-            openModal(name, description);
+        projectContainer.innerHTML = '';
+        projects.forEach(function (project) {
+            var projectItem = document.createElement('li');
+            projectItem.classList.add('project-item');
+            projectItem.innerText = project.name;
+            projectItem.addEventListener('click', function () {
+                openModal(project.name, project.description);
+            });
+            projectContainer.appendChild(projectItem);
         });
-        projectContainer.appendChild(projectItem);
     }
 
     // 모달 열기 함수
@@ -47,6 +72,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     document.getElementById('projects-link').addEventListener('click', function () {
         showSection('project-list');
+        updateProjectList(); // 프로젝트 리스트 섹션을 열 때 프로젝트 리스트 업데이트
     });
     document.getElementById('create-link').addEventListener('click', function () {
         showSection('create-project');
@@ -61,6 +87,31 @@ document.addEventListener('DOMContentLoaded', function () {
             } else {
                 section.classList.add('hidden');
             }
+        });
+    }
+
+    // 검색창 이벤트 핸들러 추가
+    document.getElementById('search-bar').addEventListener('input', function () {
+        var searchQuery = this.value.toLowerCase();
+        showSection('project-list');
+        filterProjects(searchQuery);
+    });
+
+    // 프로젝트 필터링 함수
+    function filterProjects(query) {
+        var projectContainer = document.getElementById('project-container');
+        projectContainer.innerHTML = '';
+        var filteredProjects = projects.filter(function (project) {
+            return project.name.toLowerCase().includes(query);
+        });
+        filteredProjects.forEach(function (project) {
+            var projectItem = document.createElement('li');
+            projectItem.classList.add('project-item');
+            projectItem.innerText = project.name;
+            projectItem.addEventListener('click', function () {
+                openModal(project.name, project.description);
+            });
+            projectContainer.appendChild(projectItem);
         });
     }
 });
